10b2adf186c9475099804240479f3363a825fc8f
kon.kalaitzidis <kon.kalaitzidis@gmail.com>
2024-12-03 17:40:50 +0100

    add link preview

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,10 +9,10 @@ export const metadata: Metadata = {
 export default function Contact() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">Contact</h1>
+      <h1 className="mb-8 text-2xl font-medium tracking-tight">Want to talk?</h1>
       <div className="prose prose-neutral dark:prose-invert">
       <p>
-        Want to talk? Reach out to me via email at <span style={{ color: "#0077b5"}}> kon.kalaitzidis-at-gmail.com </span>or connect with me on {" "} 
+        Reach out to me via email at <span style={{ color: "#0077b5"}}> kon.kalaitzidis-at-gmail.com </span>or connect with me on {" "} 
         <a href="https://www.linkedin.com/in/konstantinoskalaitzidis" target="_blank" rel="noopener noreferrer" style={{ color: "#0077b5", fontWeight: "bold"}}>
           LinkedIn<span style={{ marginLeft: "4px" }}></span>
         </a>🔗.
--- app/page.tsx
+++ app/page.tsx
@@ -3,55 +3,63 @@ import { socialLinks } from "./config";
 
 export default function Page() {
   return (
+    <>
+      {/* Open Graph Tags */}
+      <meta property="og:title" content="Kostas Kalaitzidis - Machine Learning Engineer" />
+      <meta property="og:description" content="Passionate about leveraging the latest advancements in AI for impact-driven research in life sciences." />
+      <meta property="og:image" content="https://kostaskal.com/home-preview-image.jpg" />
+      <meta property="og:url" content="https://kostaskal.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Kostas Kalaitzidis" />
+ 
     <section>
-      <a>
-        <Image
-          src="/me_KI.png"
-          alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
-          unoptimized
-          width={160}
-          height={160}
-          priority
-        />
-      </a>
+        <a>
+          <Image
+            src="/me_KI.png"
+            alt="Profile photo"
+            className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+            unoptimized
+            width={160}
+            height={160}
+            priority />
+        </a>
 
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        Hi 👋🏼, Kostas here.
-      </h1>
+        <h1 className="mb-8 text-2xl font-medium tracking-tight">
+          Hi 👋🏼, Kostas here.
+        </h1>
 
-      <div className="prose prose-neutral dark:prose-invert">
-        <p>
-          I'm a Machine Learning Engineer 🤖 🩻
-        </p>
-        <p>
-        I have a MSc degree in {" "}
-          <a
-            target="_blank"
-            href="https://education.ki.se/programme/5hi17-joint-masters-programme-in-health-informatics"
-          >
-            Health Informatics from the Karolinska Institute and Stockholm University
-          </a> with a background in Machine Learning and Computer Science.
-        Passionate about leveraging the latest advancements in AI for impact-driven research in life sciences. 
-        {/* Motivated, and team-oriented with experience in Sweden, Ireland, the US, and Greece. 
-        Ex-TEDx Curator with a history of cross-disciplinary collaboration in fast-paced, multicultural settings. 
-        Completed mandatory military service. */}
-        </p>
-        <p>
-          Want to tell me something? {" "}
-            <a href="/contact" style={{ color: "#0077b5", fontWeight: "bold"}}>
-            Contact
+        <div className="prose prose-neutral dark:prose-invert">
+          <p>
+            I'm a Machine Learning Engineer 🤖 🩻
+          </p>
+          <p>
+            I have a MSc degree in {" "}
+            <a
+              target="_blank"
+              href="https://education.ki.se/programme/5hi17-joint-masters-programme-in-health-informatics"
+            >
+              Health Informatics from the Karolinska Institute and Stockholm University
+            </a> with a background in Machine Learning and Computer Science.
+            Passionate about leveraging the latest advancements in AI for impact-driven research in life sciences.
+            {/* Motivated, and team-oriented with experience in Sweden, Ireland, the US, and Greece.
+    Ex-TEDx Curator with a history of cross-disciplinary collaboration in fast-paced, multicultural settings.
+    Completed mandatory military service. */}
+          </p>
+          <p>
+            Want to tell me something? {" "}
+            <a href="/contact" style={{ color: "#0077b5", fontWeight: "bold" }}>
+              Contact
             </a>
-        </p>
-        {/* <p>
-          What I'm working on now: {" "} 
-          <a
-            target="_blank"
-            href="">
-            Now page
-          </a>
-        </p> */}
-      </div>
-    </section>
+          </p>
+          {/* <p>
+      What I'm working on now: {" "}
+      <a
+        target="_blank"
+        href="">
+        Now page
+      </a>
+    </p> */}
+        </div>
+      </section></>
   );
 }
