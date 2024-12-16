f8062b93373bdc0972b1db95ac6762f119ff717d
kon.kalaitzidis <kon.kalaitzidis@gmail.com>
2024-12-16 14:48:31 +0100

    add analytics

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,3 @@
-import { useEffect } from "react";
-
-// useEffect(() => {
-//   const script = document.createElement("script");
-//   script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//   })(window,document,'script','dataLayer','GTM-N3F59665');`;
-//   document.head.appendChild(script);
-// }, []);
-
 import React from "react";
 import type { Metadata } from "next";
 // import { ImageGrid } from "app/components/image-grid";
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 import { ThemeProvider } from "./components/theme-switch";
 import { metaData } from "./config";
+import Script from 'next/script'
+import React from "react";
 
 export const metadata: Metadata = {
   metadataBase: new URL(metaData.baseUrl),
@@ -54,42 +56,53 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cx(GeistSans.variable, GeistMono.variable)}>
-      <head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          href="/rss.xml"
-          title="RSS Feed"
-        />
-        <link
-          rel="alternate"
-          type="application/atom+xml"
-          href="/atom.xml"
-          title="Atom Feed"
-        />
-        <link
-          rel="alternate"
-          type="application/feed+json"
-          href="/feed.json"
-          title="JSON Feed"
-        />
-      </head>
-      <body className="antialiased flex flex-col items-center justify-center mx-auto mt-2 lg:mt-8 mb-20 lg:mb-40">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[640px] w-full">
-            <Navbar />
-            {children}
-            <Footer />
-            <Analytics />
-            <SpeedInsights />
-          </main>
-        </ThemeProvider>
-      </body>
+      <React.Fragment>
+        <head>
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N3F59665');`}
+          </Script>
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            href="/rss.xml"
+            title="RSS Feed"
+          />
+          <link
+            rel="alternate"
+            type="application/atom+xml"
+            href="/atom.xml"
+            title="Atom Feed"
+          />
+          <link
+            rel="alternate"
+            type="application/feed+json"
+            href="/feed.json"
+            title="JSON Feed"
+          />
+        </head>
+        <body className="antialiased flex flex-col items-center justify-center mx-auto mt-2 lg:mt-8 mb-20 lg:mb-40">
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3F59665"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[640px] w-full">
+              <Navbar />
+              {children}
+              <Footer />
+              <Analytics />
+              <SpeedInsights />
+            </main>
+          </ThemeProvider>
+        </body>
+      </React.Fragment>
     </html>
   );
 }
