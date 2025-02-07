5a6392bc2c2691a0ebf9b0bb7b996ce7ef1b07f2
kon.kalaitzidis <kon.kalaitzidis@gmail.com>
2025-02-07 14:30:37 +0100

    hide text on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -55,13 +55,13 @@ export default function About() {
         </ul>
       </div>
 
-      <div className="mb-8">
+      {/* <div className="mb-8">
         <h2 className="mb-4 text-1xl font-medium tracking-tight"> What I'm doing now:</h2>  
         <p>Article <a href="https://www.annalsofoncology.org/article/S0923-7534(24)02935-1/fulltext" target="_blank" className="text-blue-500 underline" style={{ color: "#0077b5"}}>"The Stereotactic Radiosurgery-Brain Prognostic Index (SRS-BPI): A novel prognostic index for lung cancer patients with brain metastases eligible for SRS</a></p>
         <p>Attending course "<a href="https://github.com/konkalaitzidis" target="_blank" className="text-blue-500 underline" style={{ color: "#0077b5"}}>Applied Artificial Intelligence (AI) in Healthcare</a>" at Karolinska Institutet - 15 Credits</p>
         <p>Attending course "<a href="https://www.kth.se/student/kurser/kurs/CB207V?l=en" target="_blank" className="text-blue-500 underline" style={{ color: "#0077b5"}}>Computational Analyses of mRNA-seq, ChIP-seq and ATAC-seq"</a> at KTH Royal Institute of Technology - 7.5 Credits</p>
         <p></p>
-      </div>
+      </div> */}
 
       <div className="mb-8">
         <p>Want to tell me something? <a href="/contact" className="text-blue-500 underline" style={{ color: "#0077b5", fontWeight: "bold"}}>Contact</a></p>
