56feaced2b89562b1102808d2e4aede9c2b52171
kon.kalaitzidis <kon.kalaitzidis@gmail.com>
2025-06-04 16:08:03 +0200

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,12 +59,17 @@ export default function RootLayout({
     <html lang="en" className={cx(GeistSans.variable, GeistMono.variable)}>
       <React.Fragment>
         <head>
-          <Script id="gtm-script" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N3F59665');`}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-EHF4N57PTJ"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EHF4N57PTJ');
+            `}
           </Script>
           <link
             rel="alternate"
